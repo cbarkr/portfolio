316d032c61a7c885d8146d946dc06904ec430116
cbarkr <cbarkr@gmail.com>
2021-03-19 16:49:55 -0700

    navbar icons

--- src/Components/TopNav.jsx
+++ src/Components/TopNav.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Nav, Navbar} from "react-bootstrap";
 import {Link} from 'react-router-dom';
 import {AiFillGithub, AiFillLinkedin} from 'react-icons/ai';
+import {FaFingerprint, FaRegPaperPlane} from 'react-icons/fa';
 import Logo from '.././Logos/logo-white.svg';
 import "bootstrap/dist/css/bootstrap.min.css";
 import '.././App.css';
@@ -14,10 +15,12 @@ class TopNav extends React.Component{
                     <img src={Logo} alt="Logo"/>
                 </Navbar.Brand>
                 <Nav className="navbar-nav ml-auto">
-                    <Nav.Link as={Link} to="/about" title="About">about</Nav.Link>
-                    <Nav.Link as={Link} to="/contact" title="Contact">contact</Nav.Link>
+                    <Nav.Link as={Link} to="/about" title="About">
+                        <FaFingerprint size={20} /> about </Nav.Link>
+                    <Nav.Link as={Link} to="/contact" title="Contact">
+                        <FaRegPaperPlane size={20} /> contact </Nav.Link>
                     <a href="https://github.com/cbarkr" className="nav-link">
-                        <AiFillGithub title="GitHub" size={25}/>
+                        <AiFillGithub title="GitHub" size={25} />
                     </a>
                     <a href="https://www.linkedin.com/in/cbarkr/" className="nav-link">
                         <AiFillLinkedin title="LinkedIn" size={25} />
