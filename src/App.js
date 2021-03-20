649076a4f0eee84d76ee34afc599d3456afc97d5
cbarkr <cbarkr@gmail.com>
2021-03-19 22:27:35 -0700

    landing page updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import TopNav from "./Components/TopNav";
+import TopNav from './Components/TopNav';
 import Home from './Components/Home';
-import About from "./Components/About";
-import Contact from "./Components/Contact";
+import Work from './Components/Work';
+import About from './Components/About';
+import Contact from './Components/Contact';
 import {Route, BrowserRouter as Router} from 'react-router-dom';
 import './App.css';
 
@@ -12,7 +13,8 @@ function App() {
       <React.Fragment>
         <Router>
           <TopNav />
-          <Route exact path="/" component={Home}/>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/work" component={Work} />
           <Route exact path="/about" component={About} />
           <Route exact path="/contact" component={Contact} />
         </Router>
--- src/Components/TopNav.jsx
+++ src/Components/TopNav.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import {Nav, Navbar} from "react-bootstrap";
+import {Nav, Navbar} from 'react-bootstrap';
 import {Link} from 'react-router-dom';
 import {AiFillGithub, AiFillLinkedin} from 'react-icons/ai';
-import {FaFingerprint, FaRegPaperPlane} from 'react-icons/fa';
 import Logo from '.././Logos/logo-white.svg';
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '.././App.css';
 
 class TopNav extends React.Component{
@@ -15,15 +14,14 @@ class TopNav extends React.Component{
                     <img src={Logo} alt="Logo"/>
                 </Navbar.Brand>
                 <Nav className="navbar-nav ml-auto">
-                    <Nav.Link as={Link} to="/about" title="About">
-                        <FaFingerprint size={20} /> about </Nav.Link>
-                    <Nav.Link as={Link} to="/contact" title="Contact">
-                        <FaRegPaperPlane size={20} /> contact </Nav.Link>
+                    <Nav.Link as={Link} to="/work" title="Work">work</Nav.Link>
+                    <Nav.Link as={Link} to="/about" title="About">about</Nav.Link>
+                    <Nav.Link as={Link} to="/contact" title="Contact">contact</Nav.Link>
                     <a href="https://github.com/cbarkr" className="nav-link">
-                        <AiFillGithub title="GitHub" size={25} />
+                        <AiFillGithub title="GitHub" size={25} style={{color: 'yellowgreen'}}/>
                     </a>
                     <a href="https://www.linkedin.com/in/cbarkr/" className="nav-link">
-                        <AiFillLinkedin title="LinkedIn" size={25} />
+                        <AiFillLinkedin title="LinkedIn" size={25} style={{color: 'yellowgreen'}} />
                     </a>
                 </Nav>
             </Navbar>
