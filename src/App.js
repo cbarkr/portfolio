0b03d96a6ed46a5dbf531ba638c54da1fb57187c
cbarkr <cbarkr@gmail.com>
2021-03-22 16:02:39 -0700

    fixed scroll

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ScrollToTop from './ScrollToTop';
 import TopNav from './Components/TopNav';
 import Footer from './Components/Footer';
 import Home from './Components/Home';
@@ -12,13 +13,15 @@ function App() {
   return (
     <div className="wrapper">
       <React.Fragment>
-        <Router>
-          <TopNav />
-          <Route exact path="/" component={Home} />
-          <Route exact path="/work" component={Work} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/contact" component={Contact} />
-          <Footer />
+      <Router>
+        <ScrollToTop>
+            <TopNav />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/work" component={Work} />
+            <Route exact path="/about" component={About} />
+            <Route exact path="/contact" component={Contact} />
+            <Footer />
+          </ScrollToTop>
         </Router>
       </React.Fragment>
     </div>
--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import Aos from 'aos';
 import {AiFillGithub} from 'react-icons/ai';
 import {FiArrowUpRight} from 'react-icons/fi';
+import 'aos/dist/aos.css';
 import '.././App.css';
+
+Aos.init({duration: 2000});
 
 class Work extends React.Component{
     render(){
         return(
             <div className="container">
-                <div className="project">
+                <div className="project" data-aos="fade-right">
                         <h3>
                             <a href="https://github.com/cbarkr/PathfindingVisualizer" className="project-link" target="_blank" rel="noreferrer">Pathfinding Visualizer
                                 <AiFillGithub title="GitHub" size={25} style={{color: "lightgrey"}}/>
@@ -22,7 +26,7 @@ class Work extends React.Component{
                         <p>Made in Python and Pygame</p>
                     </div>
                 </div>
-                <div className="project">
+                <div className="project" data-aos="fade-left">
                         <h3>
                             <a href="https://flixxlist.herokuapp.com/" className="project-link" target="_blank" rel="noreferrer">FlixList Web App
                                 <FiArrowUpRight title="FlixList" size={25} style={{color: "lightgrey"}}/>
