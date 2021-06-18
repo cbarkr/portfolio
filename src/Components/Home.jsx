3670bde7f81cbe91e3f65dbd7ddf7c67d2c1885d
cbarkr <cbarkr@gmail.com>
2021-06-18 15:23:34 -0700

    added tree to interests

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,14 +1,40 @@
-import React from 'react';
-import Aos from 'aos';
+import React, {useState} from 'react';
+import {useSpring, animated} from 'react-spring';
 import {IoFingerPrint} from 'react-icons/io5';
 import {RiBracesFill} from 'react-icons/ri';
+import Aos from 'aos';
 import HomeTitle from './Home-Title';
-import 'aos/dist/aos.css';
-import '.././App.css';
+import * as Icons from './Tree/Icons';
+import {useMeasure, usePrevious} from './Tree/Helpers';
+import {Frame, Content, toggle} from './Tree/Styles';
 import Pathfinding from './Assets/Images/PathfindingVisualizer.jpg';
 import FlixList from './Assets/Images/FlixList-Search.png';
+import 'aos/dist/aos.css';
+import '.././App.css';
 
 Aos.init({duration: 2000});
+
+function Tree({ children, name, style, open = false }) {
+    const [isOpen, setOpen] = useState(open)
+    const prev = usePrevious(isOpen)
+    const [bind, { height: viewHeight }] = useMeasure()
+    let { height, opacity, transform } = useSpring({
+      from: { height: 0, opacity: 0, transform: 'translate3d(20px,0,0)' },
+      to: { height: isOpen ? viewHeight : 0, opacity: isOpen ? 1 : 0, transform: `translate3d(${isOpen ? 0 : 20}px,0,0)` }
+    })
+    const Icon = Icons[`${children ? (isOpen ? 'Minus' : 'Plus') : 'Close'}SquareO`]
+    return (
+      <Frame>
+        <Icon style={{ ...toggle, opacity: children ? 1 : 0.3 }} onClick={() => setOpen(!isOpen)} />
+        <span style={{ verticalAlign: 'middle', ...style }}>{name}</span>
+        <Content style={{ opacity, height: height.to(height => (isOpen && prev === isOpen ? 'auto' : height)) }}>
+          <animated.div style={{ transform }} {...bind}>
+            {children}
+          </animated.div>
+        </Content>
+      </Frame>
+    )
+}
 
 class Home extends React.Component{
     render(){
@@ -28,19 +54,35 @@ class Home extends React.Component{
                         <div className="interest-groups">
                             <div className="group1">
                                 <IoFingerPrint title="Personal" size={25} style={{color: '#2aa198'}} />
-                                <h5>Fine_Art</h5>
-                                <h5>Graphic_Design</h5>
-                                <h5>The_Great_Outdoors</h5>
+                                <h5>
+                                    <Tree name="Fine_Art">
+                                        <Tree name="Photography">
+                                            <br /><p>Focus in fashion and fine art photography</p><br />
+                                        </Tree>
+                                        <Tree name="Architecture" />
+                                        <Tree name="Fashion" />
+                                    </Tree>
+                                    <Tree name="Graphic_Design">
+                                        <Tree name="Designed personal logo" />
+                                        <Tree name="Graphics for highschool yearbooks" />
+                                    </Tree>
+                                    <Tree name="The_Great_Outdoors">
+                                        <Tree name="Hiking" />
+                                        <Tree name="Camping" />
+                                        <Tree name="Snowboarding" />
+                                    </Tree>
+                                </h5>
+
                             </div>
                             <div className="group2">
                                 <RiBracesFill title="Tech" size={25} style={{color: '#2aa198'}} />
                                 <h5>
-                                    <a href="https://en.wikipedia.org/wiki/Generative_adversarial_network" target="_blank" rel="noreferrer">
-                                        GAN
-                                    </a>
+                                    <Tree name="Web_Development">
+                                        <br /><p>Inspired by all things visual</p><br />
+                                    </Tree>
+                                    <Tree name="GAN" />
+                                    <Tree name="Computer_Vision" />
                                 </h5>
-                                <h5>Computer_Vision</h5>
-                                <h5>Web_Development</h5>
                             </div>
                         </div>
                     </div>
@@ -49,8 +91,7 @@ class Home extends React.Component{
                 <section className="aboutme">
                     <div className="static-container" data-aos="fade-up">
                         <h2>A little <a href="/about">about</a> me</h2>
-                        <p>I am a second year cs student who takes any opportunity to learn something new</p>
-                                
+                        <p>I take any opportunity to learn something new</p>
                         <h4 style={{float: 'left'}}>2019</h4><p> 👨‍🎓 Began studying computing science at Simon Fraser University</p>
                         <h4 style={{float: 'left'}}>2021</h4><p> 👨‍💻 [Current] </p>
                         <h4 style={{float: 'left'}}>20__</h4><p> ? </p>
---
+++ src/Components/Tree/Styles.js
@@ -0,0 +1,65 @@
+import styled, {createGlobalStyle} from 'styled-components'
+import {animated} from 'react-spring'
+
+const Global = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+  html,
+  body,
+  #root {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    background: #191b21;
+    overflow: hidden;
+    font-family: 'monospace','Roboto Mono', 'Helvetica Neue', sans-serif monospace;
+    font-size: 14px;
+    line-height: 21px;
+  }
+  html,
+  body,
+  div,
+  a,
+  i,
+  button,
+  select,
+  option,
+  optgroup,
+  hr,
+  br {
+    user-select: none;
+    cursor: default;
+  }
+  #root {
+    padding: 30px;
+  }
+`
+const Frame = styled('div')`
+  position: relative;
+  padding: 4px 0px 0px 0px;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow-x: auto;
+  vertical-align: middle;
+  color: lightgrey;
+  fill: lightgrey;
+`
+const Content = styled(animated.div)`
+  will-change: transform, opacity, height;
+  margin-left: 6px;
+  padding: 0px 0px 0px 14px;
+  border-left: 1px dashed rgba(255, 255, 255, 0.4);
+  overflow-x: auto;
+  overflow-y: hidden;
+`
+const toggle = {
+  width: '1em',
+  height: '1em',
+  marginRight: 10,
+  cursor: 'cell',
+  verticalAlign: 'middle'
+}
+
+export {Global, Frame, Content, toggle}
