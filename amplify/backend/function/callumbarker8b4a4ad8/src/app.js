9be7a550000438f36ca252a24f72004bcdcc2c2d
cbarkr <cbarkr@gmail.com>
2022-01-04 22:38:01 -0800

    update cors headers

--- amplify/backend/function/callumbarker8b4a4ad8/src/app.js
+++ amplify/backend/function/callumbarker8b4a4ad8/src/app.js
@@ -15,6 +15,7 @@ app.use(awsServerlessExpressMiddleware.eventContext())
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  res.header("Access-Control-Allow-Methods", "OPTIONS, POST")
   next()
 });
 
