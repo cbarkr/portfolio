076c2c9ee1d9e4791d993f95a59caa8effd405d1
cbarkr <cbarkr@gmail.com>
2022-05-03 15:46:37 -0700

    make callback wait

--- amplify/backend/function/callumbarker8b4a4ad8/src/index.js
+++ amplify/backend/function/callumbarker8b4a4ad8/src/index.js
@@ -5,6 +5,6 @@ const server = awsServerlessExpress.createServer(app, null);
 
 exports.handler = (event, context, callback) => {
   console.log(`EVENT: ${JSON.stringify(event)}`);
-  context.callbackWaitsForEmptyEventLoop = false;
+  // context.callbackWaitsForEmptyEventLoop = false;
   return awsServerlessExpress.proxy(server, event, context, 'PROMISE').promise;
 };
