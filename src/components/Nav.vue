88b8e3f4a46d5dda520a66dfcdbeb44f9b3e77ba
cbarkr <cbarkr@gmail.com>
2023-08-08 22:40:49 -0700

    minor nav cleanup

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -3,13 +3,13 @@
 </script>
 
 <template>
-  <nav class="flex flex-wrap items-center justify-between pb-8">
+  <div class="flex justify-between pb-8">
     <!-- Note: Nav text must match route name -->
-    <router-link to="/" class="text-md sm:text-l md:text-xl lg:text-2xl">{{ nav.home == this.$route.name ? selectedRoute : nav.home }}</router-link>
-    <router-link to="/projects" class="text-md sm:text-l md:text-xl lg:text-2xl">{{ nav.projects == this.$route.name ? selectedRoute : nav.projects }}</router-link>
-    <router-link to="/photos" class="text-md sm:text-l md:text-xl lg:text-2xl">{{ nav.photos == this.$route.name ? selectedRoute : nav.photos }}</router-link>
-    <router-link to="/words" class="text-md sm:text-l md:text-xl lg:text-2xl">{{ nav.words == this.$route.name ? selectedRoute : nav.words }}</router-link>
-  </nav>
+    <router-link to="/" class="text-md sm:text-l md:text-xl lg:text-2xl">{{ selectedRoute(nav.home) }}</router-link>
+    <router-link to="/projects" class="text-md sm:text-l md:text-xl lg:text-2xl">{{ selectedRoute(nav.projects) }}</router-link>
+    <router-link to="/photos" class="text-md sm:text-l md:text-xl lg:text-2xl">{{ selectedRoute(nav.photos) }}</router-link>
+    <router-link to="/words" class="text-md sm:text-l md:text-xl lg:text-2xl">{{ selectedRoute(nav.words) }}</router-link>
+  </div>
 </template>
 
 <script>
@@ -25,9 +25,9 @@
         },
       }
     },
-    computed: {
-      selectedRoute() {
-        return `[${this.$route.name}]`
+    methods: {
+      selectedRoute(name) {
+        return name === this.$route.name ? `[${name}]` : name
       }
     }
   }
