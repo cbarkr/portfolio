1fd30b3d5ff27c53c9e8579526d4dd5e7483893a
cbarkr <cbarkr@gmail.com>
2023-08-15 11:55:22 -0700

    rework work layout

--- src/pages/Work.vue
+++ src/pages/Work.vue
@@ -1,89 +1,52 @@
 <template>
-  <!-- TODO: Rework layout -->
-  <div class="flex flex-col sm:flex-row">
-    <!-- Contents -->
-    <div class="flex flex-col flex-wrap basis-1/2 mx-4">
-      <h1 class="flex flex-row text-2xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl mb-8">
-        CONTENTS
-        <!-- Arrow down -->
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 xl:w-14 xl:h-14"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3"
-          />
+  <div class="sm:px-16">
+    <hr />
+    <div
+      v-for="(c, index) in content"
+      :key="c.id"
+    >
+      <div @click="handleClick(index)" class="flex flex-row justify-between my-4 hover:underline">
+        <div class="text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl">{{c.name}}</div>
+        <!-- Plus -->
+        <svg v-if="!isActive(index)" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10 sm:w-12 sm:h-12 md:w-14 md:h-14 lg:w-16 lg:h-16 xl:w-18 xl:h-18">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
         </svg>
-      </h1>
-      <div
-        v-for="(c, index) in content"
-        :key="c.id"
-        @mouseover="handleContent(index)"
-        @click="handleContent(index)"
-        class="text-l md:text-xl hover:cursor-crosshair hover:underline"
-      >
-        {{ c.name }}
-        <hr class="my-2" />
-      </div>
-    </div>
+        <!-- Minus -->
+        <svg v-else xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-10 sm:w-12 sm:h-12 md:w-14 md:h-14 lg:w-16 lg:h-16 xl:w-18 xl:h-18">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 12h-15" />
+        </svg>
 
-    <!-- Content -->
-    <div class="my-8 sm:my-0 flex flex-col flex-wrap basis-1/2 mx-4">
-      <h1
-        class="flex flex-row sm:flex sm:justify-end text-2xl md:text-4xl lg:text-5xl xl:text-6xl 2xl:text-7xl mb-8"
-      >
-        <!-- Arrow down -->
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-6 h-6 sm:w-8 sm:h-8 md:w-10 md:h-10 lg:w-12 lg:h-12 xl:w-14 xl:h-14"
+      </div>
+      <div v-show="isActive(index)" class="flex flex-col justify-between">
+        <div class="flex flex-row">
+          <img :src="c.image" alt="project image" class="object-cover max-h-80 mb-4" />
+        </div>
+        <div class="text-lg lg:text-xl xl:text-2xl mb-4">{{ c.description }}</div>
+        <a
+          :href="c.link"
+          class="flex flex-row text-l md:text-xl mb-4"
+          target="_blank"
+          rel="noopener noreferrer"
         >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M19.5 13.5L12 21m0 0l-7.5-7.5M12 21V3"
-          />
-        </svg>
-        CONTENT
-      </h1>
-      <div :id="currentContent.id" class="text-l md:text-xl">{{ currentContent.name }}</div>
-      <hr class="my-2" />
-      <a
-        :href="currentContent.link"
-        class="flex flex-row text-l md:text-xl"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Project Repo
-        <!-- Arrow up right -->
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="currentColor"
-          class="w-6 h-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25"
-          />
-        </svg>
-      </a>
-      <hr class="my-2" />
-      <div class="text-l md:text-xl">{{ currentContent.description }}</div>
-      <hr class="my-2" />
-      <img :src="currentContent.image" alt="project image" class="object-cover w-full h-80" />
+          PROJECT REPO
+          <!-- Arrow up right -->
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M4.5 19.5l15-15m0 0H8.25m11.25 0v11.25"
+            />
+          </svg>
+        </a>
+      </div>
+      <hr />
     </div>
   </div>
 </template>
@@ -98,7 +61,7 @@ export default {
   name: 'WorkPage',
   data() {
     return {
-      currentContent: {},
+      activeContent: new Set(),
       content: [
         {
           name: 'PIP-BOY 3000 MK? PROTO',
@@ -144,13 +107,18 @@ export default {
       ]
     }
   },
-  mounted() {
-    this.currentContent = this.content[0]
-  },
   methods: {
-    handleContent(index) {
-      this.currentContent = this.content[index]
-    }
-  }
+    handleClick(index) {
+      if (this.isActive(index)) {
+        this.activeContent.delete(index)
+      }
+      else {
+        this.activeContent.add(index)
+      }
+    },
+    isActive(index) {
+      return this.activeContent.has(index)
+    },
+  },
 }
 </script>
