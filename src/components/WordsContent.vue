5914117b13cffa66b48d3368847eac8060f09715
cbarkr <cbarkr@gmail.com>
2023-08-17 12:46:59 -0700

    add layouts for word pages

---
+++ src/components/WordsContent.vue
@@ -0,0 +1,33 @@
+<template>
+    <!-- TODO: Improve layout -->
+    <div class="sm:px-16">
+      <div class="text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl mb-8">{{ title }}</div>
+      <!-- TODO: Add image? -->
+      <div v-for="paragraph in parsedContent" class="text-lg lg:text-xl xl:text-2xl mb-2">{{ paragraph }}</div>
+    </div>
+</template>
+
+<script>
+import { storeToRefs } from 'pinia'
+import { useWordsStore } from '../stores/words'
+
+const store = useWordsStore()
+
+export default {
+  name: 'WordsContentComponent',
+  data() {
+    return {
+      title: '',
+      content: '',
+      parsedContent: ''
+    }
+  },
+  mounted() {
+    const { words } = storeToRefs(store)
+
+    this.title = words.title
+    this.content = words.content
+    this.parsedContent = this.content.split('\n')
+  }
+}
+</script>
--- src/pages/Words.vue
+++ src/pages/Words.vue
@@ -1,15 +1,76 @@
 <template>
-  <!-- TODO: Layout -->
-  <div class="flex justify-center text-lg sm:flex sm:flex-row lg:text-xl xl:text-2xl py-8">
-    🚧 Currently under construction 🚧
-  </div>
-  <div class="flex justify-center text-lg sm:flex sm:flex-row lg:text-xl xl:text-2xl py-8">
-    Please check in later :)
+  <div class="sm:px-16">
+    <hr />
+    <div v-for="(w, index) in words" :key="w.id">
+      <div @click="handleClick(index)" class="flex flex-row justify-between my-4 hover:underline">
+        <div class="text-4xl md:text-5xl lg:text-6xl xl:text-7xl 2xl:text-8xl">{{ w.title }}</div>
+        <!-- Arrow right -->
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-10 h-10 sm:w-12 sm:h-12 md:w-14 md:h-14 lg:w-16 lg:h-16 xl:w-18 xl:h-18"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M13.5 4.5L21 12m0 0l-7.5 7.5M21 12H3"
+          />
+        </svg>
+      </div>
+      <hr />
+    </div>
   </div>
 </template>
 
 <script>
+import { storeToRefs } from 'pinia'
+import { useWordsStore } from '../stores/words'
+import WordsContent from '../components/WordsContent.vue'
+import test1Text from '../words/test1.txt?raw'
+import test2Text from '../words/test2.txt?raw'
+import test3Text from '../words/test3.txt?raw'
+
+const store = useWordsStore()
+
 export default {
-  name: 'WordsPage'
+  name: 'WordsPage',
+  components: {
+    WordsContent
+  },
+  data() {
+    return {
+      words: [
+        {
+          title: 'TEST TITLE',
+          id: 'test-title',
+          content: test1Text
+        },
+        {
+          title: 'TEST TITLE 2',
+          id: 'test-title-2',
+          content: test2Text
+        },
+        {
+          title: 'TEST TITLE 3',
+          id: 'test-title-3',
+          content: test3Text
+        },
+      ]
+    }
+  },
+  methods: {
+    handleClick(index) {
+      const wordsContent = this.words[index]
+
+      const { words } = storeToRefs(store)
+      words.title = wordsContent.title
+      words.content = wordsContent.content
+
+      this.$router.push(`words/${wordsContent.id}`)
+    }
+  }
 }
 </script>
