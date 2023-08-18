b96e2c76de78ff3d50203913645957d389d60b1a
cbarkr <cbarkr@gmail.com>
2023-08-18 10:52:42 -0700

    add comments to words store

--- src/stores/words.js
+++ src/stores/words.js
@@ -18,12 +18,12 @@ export const useWordsStore = defineStore('words', () => {
 
     try {
       // TODO: Improve
-      const wordsImport = await import(`../words/${newId}.txt?raw`)
+      const wordsImport = await import(`../words/${newId}.txt?raw`) // The txt filename must match its id
       const wordsTxt = wordsImport.default
       const wordsTxtSplit = wordsTxt.split('\n')
 
       words.id = newId
-      words.title = wordsTxtSplit[0]
+      words.title = wordsTxtSplit[0] // The txt must start with its title
       words.content = wordsTxtSplit.slice(1)
     } catch (e) {
       return false
