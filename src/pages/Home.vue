49f76054fbc4b2a9a966fafe2310d7cbb49f31fc
cbarkr <cbarkr@gmail.com>
2024-04-23 19:28:09 -0700

    minor bio change

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -153,7 +153,7 @@ export default {
     return {
       about: {
         bio: `
-          I am Callum Barker, a computing science student, software developer, and photographer based in Vancouver; 
+          Hi, I'm Callum, a computing science student, software developer, and photographer based in Vancouver; 
           interested in technology (especially in the realm of security and privacy), fashion, design, architecture, music, sights, sounds, tastes, experiences, lists, etc.; 
           outside of work and school, I can be found out on my bicycle, in thrift stores around town, at my local Muay Thai gym, or at home organizing my Pinterest boards.
           `,
