50224d0e8e9eb111b712c03dbb8c2f838f14deeb
cbarkr <cbarkr@gmail.com>
2026-01-19 14:37:12 -0800

    move icons into components

---
+++ src/components/icons/ArrowDownRight.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="currentColor"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    stroke="currentColor"
+  >
+    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 4.5l15 15m0 0V8.25m0 11.25H8.25" />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'ArrowDownRight'
+}
+</script>
---
+++ src/components/icons/ArrowUp.vue
@@ -0,0 +1,17 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="currentColor"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    stroke="currentColor"
+  >
+    <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 10.5L12 3m0 0l7.5 7.5M12 3v18" />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'ArrowUp'
+}
+</script>
